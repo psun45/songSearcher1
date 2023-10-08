30480b6f98c900abcc14a12e864ac77683caca16
Alex <atschmitt2@wisc.edu>
2023-10-08 17:56:16 +0000

    Added FrontendInterface.java

---
+++ FrontendInterface.java
@@ -0,0 +1,69 @@
+//////////////////////////////// FILE HEADER //////////////////////////////////
+//
+// Title: Frontend Interface for Song Searcher App
+// Course: CS 400 Fall 2023
+//
+// Authors: Erin Cunningham, Alex Schmitt, Kevin George
+// Emails: eecunningha2@wisc.edu, atschmitt2@wisc.edu, shinegeorge@wisc.edu
+// Lecturer: Gary Dahl
+//
+///////////////////////// ALWAYS CREDIT OUTSIDE HELP //////////////////////////
+//
+// Persons: NONE
+// Online Sources: NONE
+//
+///////////////////////////////////////////////////////////////////////////////
+
+import java.util.Scanner;
+
+/**
+ * Implements the functionality of the frontend for the Song Searcher app.
+ * 
+ * @author Erin Cunningham, Alex Schmitt, Kevin George
+ */
+public interface FrontendInterface {
+  
+  /**
+   * Constructor for the FrontendInterface class.
+   * 
+   * @param backend   A reference to the backend of the app.
+   * @param userInput A Scanner instance to read user input.
+   */
+  // public IndividualFrontendInterface (BackendInterface backend, Scanner userInput);
+
+  /**
+   * Starts the main command loop for the user by prompting user to select a command. The command
+   * loop is broken down into separate sub menu methods below this method.
+   */
+  public void mainLoop();
+
+  /**
+   * Prompts the user to specify which data file to load and loads the file.
+   * 
+   * @return String filePath The file path of the loaded file.
+   */
+  public String loadFile();
+
+  /**
+   * Prompts the user for a danceability score and lists all songs with the specified score.
+   */
+  public void listSongs();
+
+  /**
+   * Shows the average danceability score in the loaded dataset.
+   */
+  public void showAvgScore();
+
+  /**
+   * Exits the Song Searcher app.
+   */
+  public void exit();
+
+  /**
+   * Prints an invalid input message to the console.
+   * 
+   * @param message The message to be printed for invalid input.
+   */
+  public void invalidInput(String message);
+
+}
